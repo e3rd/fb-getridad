3e73fc1b503986611dcfc361a2773b1b61d7414f
Edvard Rejthar <github@edvard.cz>
2023-12-18 17:37:51 +0100

    fix #23 typo

--- launcher.js
+++ launcher.js
@@ -5,7 +5,7 @@ const LANG = {
     "en": {
         "Sponsored": "Sponsored",
         "Sponsored · Paid for by": "Sponsored · Paid for by",
-        "Suggestions": "Suggestions",
+        "Suggested": "Suggestions",
         "Suggested for you": "Suggested for you",
         "Suggested groups": "Suggested groups",
         "Suggested live gaming broadcast": "Suggested live gaming broadcast", // XX not sure with the translation
@@ -17,7 +17,7 @@ const LANG = {
     "fr": {
         "Sponsored": "Sponsorisé",
         "Sponsored · Paid for by": "Sponsorisé · Financé par",
-        "Suggestions": "Suggestions",
+        "Suggested": "Suggestions",
         "Suggested for you": "Suggestion pour vous",
         "Suggested groups": "Suggested groups", // XX
         "Suggested live gaming broadcast": "Suggested live gaming broadcast", // XX translation wrong
@@ -29,7 +29,7 @@ const LANG = {
     "cs": {
         "Sponsored": "Sponzorováno",
         "Sponsored · Paid for by": "Sponzorováno · Platí",
-        "Suggestions": "Návrhy", // XX ?
+        "Suggested": "Návrhy", // XX ?
         "Suggested for you": "Návrhy pro vás",
         "Suggested groups": "Navrhované skupiny", // XX ?
         "Suggested live gaming broadcast": "Navrhované živé herní vysílání",
